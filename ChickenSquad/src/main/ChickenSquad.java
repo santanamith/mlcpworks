c65ed55557a144bacd612ae857a815cf8f2a9f23
Percy <>
2017-01-03 19:43:36 -0500

    Este es el proyecto

---
+++ ChickenSquad/src/main/ChickenSquad.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PSantana
+ */
+public class ChickenSquad {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Este es un texto");
+    }
+}
