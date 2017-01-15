d1b34fccfca3293d2f659f3eb54f462e66c82d7d
percy <>
2017-01-15 02:18:12 -0500

    Cambios de la farmacia

---
+++ MerhyPharmacy/src/ConexionBD/ConexionBDMelanio.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConexionBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Marisol
+ * 
+ */
+public class ConexionBDMelanio {
+    private Connection con;
+    private Statement  sentSQL;
+    private ResultSet rst;
+
+
+    public ConexionBDMelanio() throws ClassNotFoundException,SQLException,InstantiationException,IllegalAccessException{
+        //cargar el controlador jdbc
+        String controlador="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        Class.forName(controlador).newInstance();
+        conectar(); //conectar con la fuente de datos
+    }
+
+    public void conectar() throws SQLException{
+        //datos necesarios para la conexion
+    String URL_bd="jdbc:sqlserver://CIBERT_POET\\CIBERT_POET;databaseName=PHARMACY_MERHY";
+    String usuario="poeta";
+    String contraseña="123";
+    //conexion con la bd
+    con=DriverManager.getConnection(URL_bd,usuario,contraseña);
+    // se crea una Statement para asi poder usar sentencias sql
+    sentSQL=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+    }   
+  
+    // metodo para ejecutar sentencia sql
+     public String ejecutar(String sql) throws SQLException
+		  {
+                      //executeUpdate nos sirve para hacer UPDATE, INSERT y ese tipo de sentencias que modifican la base de datos
+                      // no se necesita devolver algo
+                       sentSQL.executeUpdate(sql);
+                       
+		  return "";
+		  }
+     
+     //metodo para ejecutar consultas de sentencias sql en la base de datos
+     public ResultSet Consulta(String sql) throws SQLException{
+         
+         //executeQuery nos sirve solamente para hacer consultas de la base de datos, solo acepta sentencias como SELECT
+         // se necesita forzosamente devolver algo, guardar en una variable el resultado de la consulta
+        rst=sentSQL.executeQuery(sql);
+        
+       return rst;
+    }
+}
---
+++ MerhyPharmacy/src/DB/PharmacyMerhy.sql
@@ -0,0 +1,73 @@
+CREATE DATABASE PHARMACY_MERHY
+GO
+USE PHARMACY_MERHY
+GO
+CREATE TABLE Tipo(
+	idtipo int  NOT NULL,
+	nombres varchar(50) NOT NULL,
+	primary key(idtipo)
+)
+go 
+
+CREATE TABLE producto (
+  id_producto int NOT NULL identity(1,1),
+  nombre_producto varchar(100) NOT NULL,
+  idtipo int not null,
+  precio float NOT NULL,
+  caducidad date NOT NULL,
+  existencia int NOT NULL,
+  descripcion varchar(150) NOT NULL,
+  especificaciones varchar(150) NOT NULL,
+  PRIMARY KEY (id_producto),
+  foreign key (idtipo) references tipo(idtipo)
+)
+GO
+CREATE TABLE Cliente(
+	id_cliente int NOT NULL identity(1,1),
+	nombres varchar(50) NOT NULL,
+	direccion varchar(50) NOT NULL,
+	primary key(id_cliente)
+)
+go
+CREATE TABLE usuario (
+  id_usuario int NOT NULL IDENTITY(1,1),
+  nombre varchar(50) NOT NULL,
+  apellido_paterno varchar(50) NOT NULL,
+  apellido_materno varchar(50) NOT NULL,
+  dni char(8)not null,
+  usuario varchar(25) NOT NULL,
+  password varchar(22) NOT NULL,
+  nivel int NOT NULL,
+  PRIMARY KEY (Id_usuario)
+)
+GO
+create table tipocomprobante(
+id_tipocomprobante int,
+comprobante varchar(50),
+primary key (id_tipocomprobante)
+)
+go
+ CREATE TABLE COMPROBANTE (
+ID_COMPROBANTE		CHAR(7)		NOT NULL,--PK
+id_tipocomprobante	INT			NOT NULL,
+ID_CLIENTE			INT			NOT NULL,--FK
+id_usuario			INT			NOT NULL,--FK		--- CODIGO DE EMPLEADO
+PagoTotal			float		not null,
+FECHA_P				DATETIME	NOT NULL,--FECHA DE PAGO
+PRIMARY KEY(ID_COMPROBANTE),
+FOREIGN KEY (id_tipocomprobante) REFERENCES tipocomprobante(id_tipocomprobante),	
+FOREIGN KEY (ID_CLIENTE) REFERENCES CLIENTE(id_cliente),
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)	
+)
+GO
+
+CREATE TABLE DETALLE_COMPROBANTE(
+NUM_DETALLE_C	INT			NOT NULL,--PK
+ID_COMPROBANTE	CHAR(7)		NOT NULL,--PK
+id_producto		int			NOT NULL,--FK
+CANTIDAD		int			NOT NULL,
+PRECIO			FLOAT		NOT NULL,
+PRIMARY KEY(NUM_DETALLE_C,ID_COMPROBANTE),
+FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+)		
+go
